7676bc24fcba52873f9b71462b83e659431342a7
Shamas-Ur-Rehman <shamasurrehman509@gmail.com>
2025-01-10 23:21:46 +0500

    Change in Card image

--- src/components/From.jsx
+++ src/components/From.jsx
@@ -237,7 +237,7 @@ const Form = () => {
             </div>
             <div>
               <label htmlFor="CardImage" className="block text-sm font-medium text-gray-700">
-                Card Shamas Image
+                Card Holding Image
               </label>
               <input
                 id="CardImage"
